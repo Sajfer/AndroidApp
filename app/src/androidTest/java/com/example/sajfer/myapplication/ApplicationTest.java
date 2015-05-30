faf2ca0f62e4b7772130910935f14ae536894a38
sajfer <sajfer@gmail.com>
2015-05-30 15:08:31 +0200

    Added android system image

--- app/src/androidTest/java/com/example/sajfer/myapplication/ApplicationTest.java
+++ app/src/androidTest/java/com/example/sajfer/myapplication/ApplicationTest.java
@@ -7,7 +7,24 @@ import android.test.ApplicationTestCase;
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
+
+    private Application app;
+
     public ApplicationTest() {
         super(Application.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        app = getApplication();
+    }
+
+    public void testPreconditions() {
+        assertNotNull("mFirstTestActivity is null", app);
+    }
+
+    public void test1() {
+
     }
 }
---
+++ app/src/androidTest/java/com/example/sajfer/myapplication/MainActivityTest.java
@@ -0,0 +1,35 @@
+package com.example.sajfer.myapplication;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
+
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    private MainActivity activity;
+    private TextView text;
+
+    public MainActivityTest() {
+        super(MainActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        activity = getActivity();
+        text = (TextView)activity.findViewById(R.id.tv_id);
+    }
+
+    public void testPreconditions() {
+        assertNotNull("mFirstTestActivity is null", activity);
+    }
+
+    public void test1() {
+        final String expected = activity.getString(R.string.textViewString);
+        final String actual = text.getText().toString();
+        assertEquals(expected, actual);
+    }
+}
